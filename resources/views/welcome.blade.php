0106627556533b3d543a91174f4e1c8ccee3204f
le tan <tanltps04690@gmail.com>
2022-10-12 11:31:43 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,11 @@
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="javascript:void(0);" class="text-sm text-gray-700 dark:text-gray-500 underline" onclick="document.getElementById('logout').submit()">Logout</a>
+
+                        <form id="logout" action="{{url('logout')}}" method="post">
+                            @csrf
+                         </form>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
