08e517aaf1d5f671506b9e1f677b22d5f4bfbf57
Tan Le <tanltps04690@gmail.com>
2022-11-30 15:56:43 +0700

    update

--- database/migrations/2022_11_30_070249_update_request_status.php
+++ database/migrations/2022_11_30_070249_update_request_status.php
@@ -17,7 +17,7 @@ return new class extends Migration
     {
         Schema::table('request_statuses', function (Blueprint $table) {
             //
-            $table->dropConstrainedForeignIdFor(Partner::class);
+            // $table->dropConstrainedForeignIdFor(Partner::class);
             $table->foreignIdFor(User::class)->after('id')->constrained()->cascadeOnDelete();
         });
     }
